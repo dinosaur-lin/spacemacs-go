360cd12e725a5fdfc6a03475afce5da1d3ea594d
Yunfeng Lin <Yunfeng@Yunfeng.(none)>
2014-12-14 13:28:42 -0500

    golang config layer for spacemacs

---
+++ packages.el
@@ -0,0 +1,25 @@
+(defvar go-packages
+  '(
+    go-mode
+    company
+    company-go
+    ))
+
+(defvar go-excluded-packages '()
+  "List of packages to exclude.")
+
+(defun go/init-go-mode ()
+  (use-package go-mode
+    :defer t
+    :init
+    (progn
+      (add-hook 'before-save-hook #'gofmt-before-save)
+      (add-hook 'go-mode-hook 'company-mode)
+      (add-hook 'go-mode-hook (lambda ()
+                               (set (make-local-variable 'company-backends) '(company-go))
+                               (company-mode)))
+      (add-hook 'go-mode-hook (lambda ()
+                                (auto-complete-mode -1)))
+;      (require 'go-autocomplete)
+;      (require 'auto-complete-config)
+      (require 'go-mode-autoloads))))
