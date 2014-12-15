8c4ab2f7f65a249ab17e611f1dbbeee7032ddf7e
Yunfeng Lin <Yunfeng@Yunfeng.(none)>
2014-12-14 22:04:53 -0500

    revise code

--- packages.el
+++ packages.el
@@ -14,12 +14,10 @@
     :init
     (progn
       (add-hook 'before-save-hook #'gofmt-before-save)
-      (add-hook 'go-mode-hook 'company-mode)
       (add-hook 'go-mode-hook (lambda ()
-                               (set (make-local-variable 'company-backends) '(company-go :with company-dabbrev-code))
-                               (company-mode)))
+                                (set (make-local-variable 'company-backends)
+                                     '((company-go company-dabbrev-code)))
+                                (company-mode)))
       (add-hook 'go-mode-hook (lambda ()
                                 (auto-complete-mode -1)))
-;      (require 'go-autocomplete)
-;      (require 'auto-complete-config)
       (require 'go-mode-autoloads))))
